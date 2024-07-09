075ab24a8c186930a95719e41022f192842c1229
Humberto Yupanqui Apaza <yupanquidev@gmail.com>
2024-07-08 19:30:26 -0500

    chore: Update WezTerm configuration and cursor style

--- terminal/wezterm/wezterm.lua
+++ terminal/wezterm/wezterm.lua
@@ -14,38 +14,15 @@ config.harfbuzz_features = fonts.harfbuzz_features
 -- Color
 config.colors = colors.colors
 
-return {
-  default_prog = {"wsl.exe", "-d", "Debian", "--cd", "~"},
-  enable_tab_bar = false,
-  use_fancy_tab_bar = false,
-  hide_tab_bar_if_only_one_tab = false,
-  tab_bar_at_bottom = true,
-  tab_max_width = 20,
-  window_decorations = "NONE",
-  cursor_blink_ease_in = "Linear",
-  cursor_blink_ease_out = "Linear",
-  cursor_thickness = 1.8,
-  force_reverse_video_cursor = true,
-  font_size = 10.0,
-	colors = {
-		foreground = "#dcd7ba",
-		background = "#1f1f28",
+-- Cursor
+config.default_cursor_style = 'BlinkingBar'
 
-		cursor_bg = "#c8c093",
-		cursor_fg = "#c8c093",
-		cursor_border = "#c8c093",
-
-		selection_fg = "#c8c093",
-		selection_bg = "#2d4f67",
-
-		scrollbar_thumb = "#16161d",
-		split = "#16161d",
-
-		ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
-		brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
-		indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
-	},
-
-}
+config.enable_tab_bar = false
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = true
+config.tab_max_width = 20
+config.cursor_thickness = 1.8
+config.force_reverse_video_cursor = true
 
 return config
