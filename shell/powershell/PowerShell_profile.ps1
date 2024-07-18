a5782a636cdeda5f817d540c223077dcb2766d56
Humberto Yupanqui Apaza <yupanquidev@gmail.com>
2024-07-18 09:43:05 -0500

    feat: Add aliases for Brave and Figma, and set an alias for 'touch' command

--- shell/powershell/PowerShell_profile.ps1
+++ shell/powershell/PowerShell_profile.ps1
@@ -4,8 +4,16 @@ oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\yupanquidev.omp.json" | Inv
 # Icons
 Import-Module -Name Terminal-Icons
 
-# Alias
-function brave { start arc }
+# Aliases
+# New Aliases
+New-Alias brave "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
+New-Alias figma "C:\Users\yupan\AppData\Local\Figma\Figma.exe"
+
+# Set Aliases
+Set-Alias touch ni
+
+# Function Aliases
+function bravei { brave --incognito }
 function arc { start arc }
 
 # PNPM
@@ -16,4 +24,4 @@ fnm env --use-on-cd | Out-String | Invoke-Expression
 
 Set-PSReadLineOption -PredictionViewStyle ListView
 
-clear
+#clear
